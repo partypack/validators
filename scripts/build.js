0fd2af9ccd09cee7adda63a66b29aa695f2d325e
cjayross <calvinjayross@gmail.com>
2022-08-16 16:39:38 -0500

    touched up package.json

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,5 @@
 const { run, silent } = require('./index');
 
-run('yarn run tsc -p tsconfig.json --emitDeclarationOnly');
-silent('yarn run tscpaths -p tsconfig.json -s lib -o dist');
-run('yarn run rollup -c');
+run('pnpm tsc -p tsconfig.json --emitDeclarationOnly');
+silent('pnpm tscpaths -p tsconfig.json -s lib -o dist');
+run('pnpm rollup -c');
